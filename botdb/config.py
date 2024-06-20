481f0c80535e6d4934e7c37bf0b507407916fd56
Андрей <150895022+Lekodar@users.noreply.github.com>
2024-06-20 09:50:05 +0500

    Update config.py

--- botdb/config.py
+++ botdb/config.py
@@ -2,7 +2,7 @@ import logging
 from aiogram import Bot, Dispatcher
 
 # Объявляем переменные
-TOKEN = 'сюда токен бота'
+TOKEN = ''
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
